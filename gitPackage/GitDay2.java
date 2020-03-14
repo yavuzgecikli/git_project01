3fca064ee0732f1a0f64ba805c49de8d80a63e0e
Yavuz <yavuzgecikli@gmail.com>
2020-03-14 15:58:10 -0400

    mmmmmm

--- gitPackage/GitDay2.java
+++ gitPackage/GitDay2.java
@@ -1,5 +1,12 @@
 package gitPackage;
 
 public class GitDay2 {
+	public static void main(String[] args) {
+		
+		System.out.println(" Future is soon and perfect");
+		System.out.println(" Hello git users");
+		
+		
+	}
 
 }
--- gitPackage/NextClass.java
+++ gitPackage/NextClass.java
@@ -1,5 +1,9 @@
 package gitPackage;
 
 public class NextClass {
+	
+	
+	}
+	
 
-}
+
